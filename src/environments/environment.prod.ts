e4c6f39c50043f262038839f2614ce4dd79a0eb6
MIke Glazebrook <mikefolio@gmail.com>
2020-07-14 07:18:53 -0600

    updating firebase connection
    allowing user signup and login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
+  firebaseAPIKey: 'AIzaSyDUOvQsCy45f0uVk98YXVlgAXJKxI8553w'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
+  firebaseAPIKey: 'AIzaSyDUOvQsCy45f0uVk98YXVlgAXJKxI8553w'
 };
 
 /*
